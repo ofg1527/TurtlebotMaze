d702404d66909160152ed6dd36efe55c652b721f
ofg1527 <ofg1527@hotmail.com>
2016-06-08 13:19:09 +0300

    Duvarı görünce durup dönmesi ve bütün labirenti dolaşması gerek, çıkış noktasına gelince(istenilen yer) durması lazım.
    solve_maze adında fonksiyon oluşturuldu ve bu fonksiyon üzerinden
    program çalıştırılıyor.Programın eksikleri ve yapılmak istenenler, duvar
    görülünce bazen duruyor fakat bazen durmuyor ,istenilenden fazla
    dönüyor.

---
+++ mazesolver/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/lib:/home/omer/ros_workspace/sandbox/mazesolver/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/lib/pkgconfig:/home/omer/ros_workspace/sandbox/mazesolver/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/omer/ros_workspace/sandbox/mazesolver/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/omer/ros_workspace/sandbox/mazesolver:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
---
+++ mazesolver/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/omer/ros_workspace/sandbox/mazesolver/lib/libgtest_main.pdb"
+  "/home/omer/ros_workspace/sandbox/mazesolver/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
